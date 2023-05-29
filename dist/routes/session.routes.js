06bcef9e6e19921655cf93b3e823f6985fcbcd5e
Ana Carla <anacarlaa16@gmail.com>
2023-05-29 12:34:11 -0300

    tsc fix

--- dist/routes/session.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const session_controller_1 = require("../controllers/session.controller");
-const middlewares_1 = require("../middlewares");
-const user_schemas_1 = require("../schemas/user.schemas");
-const sessionRouter = (0, express_1.Router)();
-sessionRouter.post('', (0, middlewares_1.validateSchemaMiddleware)(user_schemas_1.userLoginSchema), session_controller_1.createSessionController);
-exports.default = sessionRouter;

918c57e3b7172377f7dc8b9e53a7dfdbdf3a53ee
Ana Carla <anacarlaa16@gmail.com>
2023-06-01 16:51:06 -0300

    fix express-async-errors import and add cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import 'express-async-errors'
 import express from 'express'
 import router from './routes'
+import cors from 'cors'
+import { Request, Response, NextFunction } from 'express'
 
-//import handdleError from './errors/handleError'
+
+import handdleError from './errors/handleError'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use('/',router)
-//app.use(handdleError)
+app.use(handdleError)
 
 export default app
